d6919b5475512e6c52f3916bd1b37b0f6c0ddd1f
scholl.maarten@gmail.com <scholl.maarten@gmail.com>
2019-09-26 00:40:17 +0100

    added base exception class for library

---
+++ esl/exception.cpp
@@ -0,0 +1,74 @@
+/// \file   exception.cpp
+///
+/// \brief
+///
+/// \authors    Maarten P. Scholl
+/// \date       2019-09-24
+/// \copyright  Copyright 2017-2019 The Institute for New Economic Thinking,
+///             Oxford Martin School, University of Oxford
+///
+///             Licensed under the Apache License, Version 2.0 (the "License");
+///             you may not use this file except in compliance with the License.
+///             You may obtain a copy of the License at
+///
+///                 http://www.apache.org/licenses/LICENSE-2.0
+///
+///             Unless required by applicable law or agreed to in writing,
+///             software distributed under the License is distributed on an "AS
+///             IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+///             express or implied. See the License for the specific language
+///             governing permissions and limitations under the License.
+///
+///             You may obtain instructions to fulfill the attribution
+///             requirements in CITATION.cff
+///
+#ifdef WITH_PYTHON
+///
+/// https://docs.python.org/3.7/c-api/intro.html#include-files
+///
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+#endif
+
+#include <esl/exception.hpp>
+
+
+namespace esl {
+    exception::exception(const std::string &message)
+        : message_(message)
+    {
+
+    }
+
+    const char *exception::what() const override
+    {
+        return message_.str();
+    }
+}
+
+
+#ifdef WITH_PYTHON
+#include <boost/python.hpp>
+namespace esl {
+    ///
+    /// \brief
+    ///
+    /// \param e
+    void translate_exception(const exception &e)
+    {
+        PyErr_SetString(PyExc_UserWarning, e.message_);
+    }
+
+
+    using namespace boost::python;
+    BOOST_PYTHON_MODULE(exception)
+    {
+        def("exception", exception)
+            def("message", exception::what())
+            ;
+
+        register_exception_translator<exception>(translate_exception);
+    }
+
+}  // namespace esl
+#endif
