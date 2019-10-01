2d32f56aea55e2d4b0956d13948639eba067e870
scholl.maarten@gmail.com <scholl.maarten@gmail.com>
2019-10-01 16:14:43 +0100

    added location class

--- esl/geography/location.hpp
+++ esl/geography/location.hpp
@@ -4,7 +4,8 @@
 ///
 /// \authors    Maarten P. Scholl
 /// \date       2018-04-28
-/// \copyright  Copyright 2017-2019 The Institute for New Economic Thinking, Oxford Martin School, University of Oxford
+/// \copyright  Copyright 2017-2019 The Institute for New Economic Thinking,
+///             Oxford Martin School, University of Oxford
 ///
 ///             Licensed under the Apache License, Version 2.0 (the "License");
 ///             you may not use this file except in compliance with the License.
@@ -12,23 +13,22 @@
 ///
 ///                 http://www.apache.org/licenses/LICENSE-2.0
 ///
-///             Unless required by applicable law or agreed to in writing, software
-///             distributed under the License is distributed on an "AS IS" BASIS,
-///             WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-///             See the License for the specific language governing permissions and
-///             limitations under the License.
+///             Unless required by applicable law or agreed to in writing,
+///             software distributed under the License is distributed on an "AS
+///             IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+///             express or implied. See the License for the specific language
+///             governing permissions and limitations under the License.
 ///
-///             You may obtain instructions to fulfill the attribution requirements in CITATION.cff
+///             You may obtain instructions to fulfill the attribution
+///             requirements in CITATION.cff
 ///
 #ifndef ESL_LOCATION_HPP
 #define ESL_LOCATION_HPP
 
 namespace esl::geography {
     struct location
-    {
-
-    };
-}
+    {};
+}  // namespace esl::geography
 
 
-#endif//ESL_LOCATION_HPP
+#endif  // ESL_LOCATION_HPP
