222a4133b1b55bc0185aa0d6ee72b247f9ccbed9
scholl.maarten@gmail.com <scholl.maarten@gmail.com>
2019-10-01 16:17:41 +0100

    added cash class and reformatted code using Clang

--- esl/economics/cash.hpp
+++ esl/economics/cash.hpp
@@ -1,47 +1,48 @@
-///// \file   cash.hpp
-/////
-///// \brief
-/////
-///// \authors    Maarten P. Scholl
-///// \date       2018-04-28
-///// \copyright  Copyright 2017-2019 The Institute for New Economic Thinking, Oxford Martin School, University of Oxford
-/////
-/////             Licensed under the Apache License, Version 2.0 (the "License");
-/////             you may not use this file except in compliance with the License.
-/////             You may obtain a copy of the License at
-/////
-/////                 http://www.apache.org/licenses/LICENSE-2.0
-/////
-/////             Unless required by applicable law or agreed to in writing, software
-/////             distributed under the License is distributed on an "AS IS" BASIS,
-/////             WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-/////             See the License for the specific language governing permissions and
-/////             limitations under the License.
-/////
-/////             You may obtain instructions to fulfill the attribution requirements in CITATION.cff
-/////
+/// \file   cash.hpp
+///
+/// \brief
+///
+/// \authors    Maarten P. Scholl
+/// \date       2018-04-28
+/// \copyright  Copyright 2017-2019 The Institute for New Economic Thinking,
+///             Oxford Martin School, University of Oxford
+///
+///             Licensed under the Apache License, Version 2.0 (the "License");
+///             you may not use this file except in compliance with the License.
+///             You may obtain a copy of the License at
+///
+///                 http://www.apache.org/licenses/LICENSE-2.0
+///
+///             Unless required by applicable law or agreed to in writing,
+///             software distributed under the License is distributed on an "AS
+///             IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+///             express or implied. See the License for the specific language
+///             governing permissions and limitations under the License.
+///
+///             You may obtain instructions to fulfill the attribution
+///             requirements in CITATION.cff
+///
 #ifndef ESL_CASH_HPP
 #define ESL_CASH_HPP
 
+#include <esl/algorithms.hpp>
 #include <esl/economics/currency.hpp>
 #include <esl/economics/money.hpp>
-#include <esl/quantity.hpp>
-#include <esl/algorithms.hpp>
 #include <esl/economics/tangibility.hpp>
+#include <esl/quantity.hpp>
 
 namespace esl::economics {
     ///
-    /// \brief  (Financial) cash, that for example can be exchanged electronically. For tangible (physical) money, such as notes and coins, see physical_money
+    /// \brief  (Financial) cash, that for example can be exchanged
+    /// electronically. For tangible (physical) money, such as notes and coins,
+    /// see physical_money
     ///
     struct cash
     : public money
     , public intangible
     {
-        explicit cash(currency denomination)
-        : money(denomination)
-        {
-
-        }
+        explicit cash(currency denomination) : money(denomination)
+        {}
 
         virtual ~cash() = default;
 
@@ -52,7 +53,7 @@ namespace esl::economics {
             return stream_.str();
         }
 
-        constexpr bool operator == (const cash &c) const
+        constexpr bool operator==(const cash &c) const
         {
             return this->denomination == c.denomination;
         }
@@ -84,18 +85,19 @@ namespace esl::economics {
                          denomination);
         }
     };
-}
+}  // namespace esl::economics
 
 
 namespace std {
     template<>
     struct hash<esl::economics::cash>
     {
-        std::size_t operator()(const esl::economics::cash& c) const
+        std::size_t operator()(const esl::economics::cash &c) const
         {
-            return std::hash<esl::economics::currency>()(c.denomination);// ^ c.quantity;
+            return std::hash<esl::economics::currency>()(
+                c.denomination);
         }
     };
-}
+}  // namespace std
 
-#endif//ESL_CASH_HPP
+#endif  // ESL_CASH_HPP
