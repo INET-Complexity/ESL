3272f69cae883388551154dcaeb117a68da27aab
scholl.maarten@gmail.com <scholl.maarten@gmail.com>
2019-10-06 19:50:50 +0100

    reformat for doxygen

--- test/test_python_time.py
+++ test/test_python_time.py
@@ -1,11 +1,11 @@
 """
-/// \file   test_python_time.py
+/// @file   test_python_time.py
 ///
-/// \brief  tests the time classes as exported to python
+/// @brief  tests the time classes as exported to python
 ///
-/// \authors    Maarten P. Scholl
-/// \date       2019-09-04
-/// \copyright  Copyright 2017-2019 The Institute for New Economic Thinking,
+/// @authors    Maarten P. Scholl
+/// @date       2019-09-04
+/// @copyright  Copyright 2017-2019 The Institute for New Economic Thinking,
 ///             Oxford Martin School, University of Oxford
 ///
 ///             Licensed under the Apache License, Version 2.0 (the "License");
