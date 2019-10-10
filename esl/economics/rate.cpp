ccfa1c791a4ffb0ef5597f78e5b1732adb34abc3
scholl.maarten@gmail.com <scholl.maarten@gmail.com>
2019-10-10 21:29:45 +0100

    export signed rate to python

--- esl/economics/rate.cpp
+++ esl/economics/rate.cpp
@@ -23,3 +23,39 @@
 ///             requirements in CITATION.cff
 ///
 #include <esl/economics/rate.hpp>
+
+
+
+#ifdef WITH_PYTHON
+#include <boost/python.hpp>
+
+using namespace boost::python;
+
+BOOST_PYTHON_MODULE(price)
+{
+    class_<esl::economics::rate<std::int64_t>>("rate_int64",
+                                  init<std::int64_t, std::int64_t>())
+
+        .def(self == self)
+        .def(self != self)
+;
+/*
+        .def(self <  self)
+        .def(self <= self)
+        .def(self >  self)
+        .def(self >= self)
+
+        .def(self +  self)
+        .def(self += self)
+        .def(self -  self)
+        .def(self -= self)
+
+        .def(self * self)
+        .def(self * self)
+        .def(self /  self)
+        .def(self /= self)
+        ;
+*/
+}
+
+#endif  // WITH_PYTHON
