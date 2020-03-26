9299f4d3d1ca6b4cd1f340a0e017aac3143877fa
Maarten P. Scholl <scholl.maarten@gmail.com>
2020-03-26 03:00:34 +0000

    add bond class

--- esl/economics/finance/bond.hpp
+++ esl/economics/finance/bond.hpp
@@ -27,8 +27,33 @@
 
 #include <variant>
 
-#include <esl/economics/interest_rate.hpp>
 #include <esl/economics/finance/security.hpp>
+#include <esl/economics/interest_rate.hpp>
+#include <esl/law/legal_person.hpp>
+
+#ifdef WITH_QL
+#include <ql/qldefines.hpp>
+#ifdef BOOST_MSVC
+#include <ql/auto_link.hpp>
+#endif
+#include <ql/cashflows/couponpricer.hpp>
+#include <ql/indexes/ibor/euribor.hpp>
+#include <ql/indexes/ibor/usdlibor.hpp>
+#include <ql/instruments/bonds/floatingratebond.hpp>
+#include <ql/instruments/bonds/zerocouponbond.hpp>
+#include <ql/pricingengines/bond/discountingbondengine.hpp>
+#include <ql/termstructures/volatility/optionlet/constantoptionletvol.hpp>
+#include <ql/termstructures/yield/bondhelpers.hpp>
+#include <ql/termstructures/yield/piecewiseyieldcurve.hpp>
+#include <ql/time/calendars/target.hpp>
+#include <ql/time/calendars/unitedstates.hpp>
+#include <ql/time/daycounters/actual360.hpp>
+#include <ql/time/daycounters/actualactual.hpp>
+#include <ql/time/daycounters/one.hpp>
+#include <ql/time/daycounters/thirty360.hpp>
+
+#endif
+
 
 
 namespace esl::economics::finance {
@@ -36,28 +61,44 @@ namespace esl::economics::finance {
     : public virtual security
     , public identifiable_as<bond>
     {
-        std::variant<nominal_interest_rate/*, indexed_interest_rate*/> coupon;
+        struct basic_coupon
+        {
+
+        };
+
+        struct fixed_coupon: basic_coupon
+        {
+            nominal_interest_rate rate;
+
+        };
+
+
+        // std::variant<nominal_interest_rate/*, indexed_interest_rate*/>
+        // coupon;
+        std::shared_ptr<basic_coupon> coupon;
+
+        esl::simulation::time_point effective;
 
         esl::simulation::time_point maturity;
 
-        /*
-        bond( esl::identity<bond> i
-                , esl::simulation::time_point maturity
-            , nominal_interest_rate coupon = nominal_interest_rate(0, esl::simulation::time_duration(252) ))
-        : property(i)
-        , security(i, )
-        , coupon(coupon)
-        , maturity(maturity)
+
+        bond(esl::identity<bond> i, esl::simulation::time_point maturity,
+             std::shared_ptr<basic_coupon> coupon, const law::legal_person &issuer )
+        : property(i), security(i, isin(issuer.primary_jurisdiction.sovereign)), coupon(coupon), maturity(maturity)
         {
 
-        }*/
+        }
+
+        std::string name() const override
+        {
+            std::stringstream stream_;
+            stream_ << "bond" << ' ' << this->identifier;
+            return stream_.str();
+        }
     };
 
-    struct convertible_bond
-    : public bond
-    {
+    struct convertible_bond : public bond
+    {};
+}  // namespace esl::economics::finance
 
-    };
-}
-
-#endif //ME_BOND_HPP
+#endif  // ME_BOND_HPP
