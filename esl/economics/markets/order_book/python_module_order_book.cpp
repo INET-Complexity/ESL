c9896854f7c7ea0539378aa6f62b7c59402e513b
Maarten P. Scholl <scholl.maarten@gmail.com>
2020-11-03 03:51:12 +0000

    separate order book implementations

--- esl/economics/markets/order_book/python_module_order_book.cpp
+++ esl/economics/markets/order_book/python_module_order_book.cpp
@@ -23,14 +23,16 @@
 ///             requirements in CITATION.cff
 ///
 #ifdef WITH_PYTHON
-#include <esl/economics/markets/order_book/python_module_order_book.hpp>
+#include <esl/economics/markets/order_book/basic_book.hpp>
+#include <esl/economics/markets/order_book/binary_tree_order_book.hpp>
+#include <esl/economics/markets/order_book/static_order_book.hpp>
 #include <esl/economics/markets/order_book/matching_engine.hpp>
-#include <esl/economics/markets/order_book/book.hpp>
+#include <esl/economics/markets/order_book/python_module_order_book.hpp>
 using namespace esl::economics::markets::order_book;
-
 
 #include <boost/python.hpp>
 using namespace boost::python;
+
 
 BOOST_PYTHON_MODULE(order_book)
 {
@@ -49,8 +51,50 @@ BOOST_PYTHON_MODULE(order_book)
         .def_readwrite("owner", &execution_report::owner)
         .def("__repr__", &execution_report::representation)
         .def("__str__", &execution_report::representation)
+        ;
 
-    ;
+    ///
+    /// \brief Export the abstract base class, so that python users too can
+    ///        implement new order books.
+    ///
+    class_<basic_book, boost::noncopyable>("basic_book", no_init)
+        .def_readwrite("reports", &basic_book::reports)
+        .def("ask", &basic_book::ask)
+        .def("bid", &basic_book::bid)
+        .def("insert", &basic_book::insert)
+        .def("cancel", &basic_book::cancel)
+        .def("display", &basic_book::display)
+        ;
+
+    ///
+    /// \brief Export the abstract base class, so that python users too can
+    ///        implement new order books.
+    ///
+    class_<basic_book, boost::noncopyable>("basic_book", no_init)
+        .def_readwrite("reports", &basic_book::reports)
+        .def("ask", &basic_book::ask)
+        .def("bid", &basic_book::bid)
+        .def("insert", &basic_book::insert)
+        .def("cancel", &basic_book::cancel)
+        .def("display", &basic_book::display)
+        ;
+
+    class_<static_order_book, bases<basic_book>>("static_order_book", init<esl::economics::markets::quote, esl::economics::markets::quote, size_t>())
+        .def_readwrite("reports", &basic_book::reports)
+        .def("ask", &basic_book::ask)
+        .def("bid", &basic_book::bid)
+        .def("insert", &basic_book::insert)
+        .def("cancel", &basic_book::cancel)
+        .def("display", &basic_book::display)
+        ;
+
+
+
+
+
+
+
+
 
 }
 
