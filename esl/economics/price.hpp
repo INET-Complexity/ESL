322aa1ce749557b7b6c85b44b9887c7137039de5
Maarten P. Scholl <scholl.maarten@gmail.com>
2020-11-05 16:03:46 +0000

    Github Actions caches build artifacts, so we keep gcc --HEAD

--- esl/economics/price.hpp
+++ esl/economics/price.hpp
@@ -25,6 +25,8 @@
 
 #include <iomanip>
 #include <cmath>
+#include <sstream>
+#include <string>
 
 #include <esl/economics/iso_4217.hpp>
 
@@ -41,7 +43,8 @@ namespace esl::economics {
     }
 
     ///
-    /// A price is a quote in terms of monetary value.
+    /// \brief  A price is a quote in terms of monetary value. Prices can be
+    ///         negative,
     ///
     struct price
     {
@@ -54,8 +57,9 @@ namespace esl::economics {
         ///
         /// \param value
         /// \param valuation
-        explicit constexpr price(int64_t value = 0,
-                                 iso_4217 valuation = iso_4217())
+        explicit constexpr price( int64_t value = 0
+                                ,  iso_4217 valuation = iso_4217()
+                                )
         : value(value), valuation(valuation)
         {
 
@@ -73,14 +77,12 @@ namespace esl::economics {
 
         }
 
-
         explicit constexpr price(double value_untruncated,
                                  const iso_4217& valuation = iso_4217())
                 : price(static_cast<int64_t>(value_untruncated * valuation.denominator), valuation)
         {
 
         }
-
 
         constexpr price(const price &p)
         : price(p.value, p.valuation)
@@ -95,13 +97,11 @@ namespace esl::economics {
 
         }
 
-
         constexpr price(double value_untruncated, const price &similar)
             : price(value_untruncated, similar.valuation)
         {
 
         }
-
 
         price &operator = (const price &p)
         {
@@ -110,12 +110,12 @@ namespace esl::economics {
             return *this;
         }
 
-        [[nodiscard]] constexpr bool operator==(const price &operand) const
+        [[nodiscard]] constexpr bool operator == (const price &operand) const
         {
             return (value == operand.value) && (valuation == operand.valuation);
         }
 
-        [[nodiscard]] constexpr bool operator!=(const price &operand) const
+        [[nodiscard]] constexpr bool operator != (const price &operand) const
         {
             return (value != operand.value) || (valuation != operand.valuation);
         }
@@ -155,7 +155,6 @@ namespace esl::economics {
             return *this;
         }
 
-
         [[nodiscard]] constexpr price operator - () const
         {
             return price(-value, valuation);
@@ -175,18 +174,18 @@ namespace esl::economics {
         }
 
         template<typename integer_t_>
-        [[nodiscard]] constexpr price operator*(const integer_t_ &scalar) const
+        [[nodiscard]] constexpr price operator * (const integer_t_ &scalar) const
         {
             static_assert(std::is_integral<integer_t_>::value);
             return price(value * scalar, valuation);
         }
 
-        [[nodiscard]] constexpr friend price operator*(const price &p, const uint64_t &operand)
+        [[nodiscard]] constexpr friend price operator * (const price &p, const uint64_t &operand)
         {
             return price(static_cast<int64_t>(p.value * operand), p.valuation);
         }
 ;
-        [[nodiscard]] constexpr friend price operator*(const uint64_t &operand, const price &p)
+        [[nodiscard]] constexpr friend price operator * (const uint64_t &operand, const price &p)
         {
             return p * operand;
         }
@@ -211,17 +210,16 @@ namespace esl::economics {
             return p.operator<<(o);
         }
 
-        //friend std::istream &operator>>(std::istream &i, const price &p)
-        //{
-        //    std::array<char, 3> iso_currency_code_;
-        //    i.read(iso_currency_code_.data(), iso_currency_code_.size());
-        //    std::int64_t amount_;
-        //    i >> amount_;
-        //    p = price(amount_, iso_currency_code_);
-        //    return i;
-        //}
 
-
+//        friend std::istream &operator>>(std::istream &i, const price &p)
+//        {
+//            std::array<char, 3> iso_currency_code_;
+//            i.read(iso_currency_code_.data(), iso_currency_code_.size());
+//            std::int64_t amount_;
+//            i >> amount_;
+//            p = price(amount_, iso_currency_code_);
+//            return i;
+//        }
 
         template<class archive_t>
         void serialize(archive_t &archive, const unsigned int version)
@@ -230,7 +228,6 @@ namespace esl::economics {
             //archive &BOOST_SERIALIZATION_NVP(value);
             //archive &boost::serialization::make_nvp(
             //    "valuation", const_cast<iso_4217 &>(valuation));
-
 
             std::stringstream stream_;
             stream_ << valuation.code[0];
@@ -259,9 +256,11 @@ namespace boost::serialization {
         }*/
 
         template<typename archive_t>
-        inline void load_construct_data(
-                archive_t &archive, esl::economics::price * t, const unsigned int file_version
-        ){
+        inline void load_construct_data( archive_t &archive
+                                       , esl::economics::price * t
+                                       , const unsigned int file_version
+                                       )
+        {
             (void) file_version;
             std::int64_t value_ = 0;
             archive >> value_;
@@ -282,231 +281,5 @@ namespace boost::mpi {
     };
 }
 #endif//WITH_MPI
-
-
-
-
-
-
-
-//#include <esl/economics/currencies.hpp>
-
-
-
-
-//#include <locale>
-
-/*
-
-constexpr esl::economics::price operator "" _AED(const char *p)
-{
-
-    //std::use_facet< std::numpunct<char> >(std::cout.getloc()).decimal_point()
-
-    enum parser_states{
-        integer_part,
-        decimal_part
-    }state_;
-
-
-    for(const char *i =p; *i != '\0'; ++i){
-
-    }
-
-
-    auto x = esl::economics::currencies::AED;
-
-    auto z = std::atof(p);
-    return esl::economics::price(123, esl::economics::currencies::AED);
-}
-
-
-"AED" | AED;
-"AFN" | AFN;
-"ALL" | ALL;
-"AMD" | AMD;
-"ANG" | ANG;
-"AOA" | AOA;
-"ARS" | ARS;
-"AUD" | AUD;
-"AWG" | AWG;
-"AZN" | AZN;
-"BAM" | BAM;
-"BBD" | BBD;
-"BDT" | BDT;
-"BGN" | BGN;
-"BHD" | BHD;
-"BIF" | BIF;
-"BMD" | BMD;
-"BND" | BND;
-"BOB" | BOB;
-"BOV" | BOV;
-"BRL" | BRL;
-"BSD" | BSD;
-"BTN" | BTN;
-"BWP" | BWP;
-"BYN" | BYN;
-"BZD" | BZD;
-"CAD" | CAD;
-"CDF" | CDF;
-"CHE" | CHE;
-"CHF" | CHF;
-"CHW" | CHW;
-"CLF" | CLF;
-"CLP" | CLP;
-"CNY" | CNY;
-"COP" | COP;
-"COU" | COU;
-"CRC" | CRC;
-"CUC" | CUC;
-"CUP" | CUP;
-"CVE" | CVE;
-"CZK" | CZK;
-"DJF" | DJF;
-"DKK" | DKK;
-"DOP" | DOP;
-"DZD" | DZD;
-"EGP" | EGP;
-"ERN" | ERN;
-"ETB" | ETB;
-"EUR" | EUR;
-"FJD" | FJD;
-"FKP" | FKP;
-"GBP" | GBP;
-"GEL" | GEL;
-"GHS" | GHS;
-"GIP" | GIP;
-"GMD" | GMD;
-"GNF" | GNF;
-"GTQ" | GTQ;
-"GYD" | GYD;
-"HKD" | HKD;
-"HNL" | HNL;
-"HRK" | HRK;
-"HTG" | HTG;
-"HUF" | HUF;
-"IDR" | IDR;
-"ILS" | ILS;
-"INR" | INR;
-"IQD" | IQD;
-"IRR" | IRR;
-"ISK" | ISK;
-"JMD" | JMD;
-"JOD" | JOD;
-"JPY" | JPY;
-"KES" | KES;
-"KGS" | KGS;
-"KHR" | KHR;
-"KMF" | KMF;
-"KPW" | KPW;
-"KRW" | KRW;
-"KWD" | KWD;
-"KYD" | KYD;
-"KZT" | KZT;
-"LAK" | LAK;
-"LBP" | LBP;
-"LKR" | LKR;
-"LRD" | LRD;
-"LSL" | LSL;
-"LYD" | LYD;
-"MAD" | MAD;
-"MDL" | MDL;
-"MGA" | MGA;
-"MKD" | MKD;
-"MMK" | MMK;
-"MNT" | MNT;
-"MOP" | MOP;
-"MRU" | MRU;
-"MUR" | MUR;
-"MVR" | MVR;
-"MWK" | MWK;
-"MXN" | MXN;
-"MXV" | MXV;
-"MYR" | MYR;
-"MZN" | MZN;
-"NAD" | NAD;
-"NGN" | NGN;
-"NIO" | NIO;
-"NOK" | NOK;
-"NPR" | NPR;
-"NZD" | NZD;
-"OMR" | OMR;
-"PAB" | PAB;
-"PEN" | PEN;
-"PGK" | PGK;
-"PHP" | PHP;
-"PKR" | PKR;
-"PLN" | PLN;
-"PYG" | PYG;
-"QAR" | QAR;
-"RON" | RON;
-"RSD" | RSD;
-"RUB" | RUB;
-"RWF" | RWF;
-"SAR" | SAR;
-"SBD" | SBD;
-"SCR" | SCR;
-"SDG" | SDG;
-"SEK" | SEK;
-"SGD" | SGD;
-"SHP" | SHP;
-"SLL" | SLL;
-"SOS" | SOS;
-"SRD" | SRD;
-"SSP" | SSP;
-"STN" | STN;
-"SVC" | SVC;
-"SYP" | SYP;
-"SZL" | SZL;
-"THB" | THB;
-"TJS" | TJS;
-"TMT" | TMT;
-"TND" | TND;
-"TOP" | TOP;
-"TRY" | TRY;
-"TTD" | TTD;
-"TWD" | TWD;
-"TZS" | TZS;
-"UAH" | UAH;
-"UGX" | UGX;
-"USD" | USD;
-"USN" | USN;
-"UYI" | UYI;
-"UYU" | UYU;
-"UZS" | UZS;
-"VEF" | VEF;
-"VND" | VND;
-"VUV" | VUV;
-"WST" | WST;
-"XAF" | XAF;
-"XAG" | XAG;
-"XAU" | XAU;
-"XBA" | XBA;
-"XBB" | XBB;
-"XBC" | XBC;
-"XBD" | XBD;
-"XCD" | XCD;
-"XDR" | XDR;
-"XOF" | XOF;
-"XPD" | XPD;
-"XPF" | XPF;
-"XPT" | XPT;
-"XSU" | XSU;
-"XUA" | XUA;
-"YER" | YER;
-"ZAR" | ZAR;
-"ZMW" | ZMW;
-"ZWL" | ZWL;
-*/
-
-
-
-
-
-
-
-
-
-
 
 #endif//ESL_PRICE_HPP
