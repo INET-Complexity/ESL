b8ee6b870b159198854f6deb646328da1e951896
Maarten P. Scholl <scholl.maarten@gmail.com>
2020-11-09 20:22:08 +0000

    build for Linux and macOS

--- esl/geography/python_module_geography.hpp
+++ esl/geography/python_module_geography.hpp
@@ -1,10 +1,10 @@
-/// \file   countries.cpp
+/// \file   python_module_geography.hpp
 ///
 /// \brief
 ///
-/// \authors    Maarten P. Scholl
-/// \date       2018-04-27
-/// \copyright  Copyright 2017-2019 The Institute for New Economic Thinking,
+/// \authors    maarten
+/// \date       2020-11-09
+/// \copyright  Copyright 2017-2020 The Institute for New Economic Thinking,
 ///             Oxford Martin School, University of Oxford
 ///
 ///             Licensed under the Apache License, Version 2.0 (the "License");
@@ -22,3 +22,8 @@
 ///             You may obtain instructions to fulfill the attribution
 ///             requirements in CITATION.cff
 ///
+#ifndef ESL_PYTHON_MODULE_GEOGRAPHY_HPP
+#define ESL_PYTHON_MODULE_GEOGRAPHY_HPP
+
+
+#endif  // ESL_PYTHON_MODULE_GEOGRAPHY_HPP
