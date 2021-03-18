95fd1d0e784b3b693ca61ab9b46455054aff70bf
Maarten P. Scholl <scholl.maarten@gmail.com>
2021-03-18 18:07:29 +0100

    backwards compatibility for <filesystem> on older platforms, fixed warnigns

--- esl/data/filesystem.hpp
+++ esl/data/filesystem.hpp
@@ -2,7 +2,7 @@
 ///
 /// \brief
 ///
-/// \authors    maarten
+/// \authors    Maarten P. Scholl
 /// \date       2021-03-18
 /// \copyright  Copyright 2017-2021 The Institute for New Economic Thinking,
 ///             Oxford Martin School, University of Oxford
@@ -25,74 +25,47 @@
 #ifndef MARKET_ECOLOGY_FILESYSTEM_HPP
 #define MARKET_ECOLOGY_FILESYSTEM_HPP
 
-#ifndef INCLUDE_STD_FILESYSTEM_EXPERIMENTAL
-
-// Check for feature test macro for <filesystem>
-#   if defined(__cpp_lib_filesystem)
-#       define INCLUDE_STD_FILESYSTEM_EXPERIMENTAL 0
-
-// Check for feature test macro for <experimental/filesystem>
-#   elif defined(__cpp_lib_experimental_filesystem)
-#       define INCLUDE_STD_FILESYSTEM_EXPERIMENTAL 1
-
-// We can't check if headers exist...
-// Let's assume experimental to be safe
-#   elif !defined(__has_include)
-#       define INCLUDE_STD_FILESYSTEM_EXPERIMENTAL 1
-
+//  test <filesystem>
+#if defined(__cpp_lib_filesystem)
+    #define INCLUDE_STD_FILESYSTEM_EXPERIMENTAL 0
+// test <experimental/filesystem>
+#elif defined(__cpp_lib_experimental_filesystem)
+    #define INCLUDE_STD_FILESYSTEM_EXPERIMENTAL 1
+// fall back on <experimental/filesystem> if __has_include is not available
+#elif !defined(__has_include)
+    #define INCLUDE_STD_FILESYSTEM_EXPERIMENTAL 1
 // Check if the header "<filesystem>" exists
-#   elif __has_include(<filesystem>)
+#elif __has_include(<filesystem>)
+    // test Visual Studio and C++17 mode
+    #ifdef _MSC_VER
+        #if __has_include(<yvals_core.h>)
+            #include <yvals_core.h>
+            #if defined(_HAS_CXX17) && _HAS_CXX17
+                #define INCLUDE_STD_FILESYSTEM_EXPERIMENTAL 0
+            #endif
+        #endif
+        // fall back to experimental, if we can't determine C++17
+        #ifndef INCLUDE_STD_FILESYSTEM_EXPERIMENTAL
+            #define INCLUDE_STD_FILESYSTEM_EXPERIMENTAL 1
+        #endif
+    #else // #ifdef _MSC_VER
+        #define INCLUDE_STD_FILESYSTEM_EXPERIMENTAL 0
+    #endif
 
-// If we're compiling on Visual Studio and are not compiling with C++17, we need to use experimental
-#       ifdef _MSC_VER
+#elif __has_include(<experimental/filesystem>)
+    #define INCLUDE_STD_FILESYSTEM_EXPERIMENTAL 1
+#else
+    #error Missing "<filesystem>" or "<experimental/filesystem>"
+#endif
 
-// Check and include header that defines "_HAS_CXX17"
-#           if __has_include(<yvals_core.h>)
-#               include <yvals_core.h>
-
-// Check for enabled C++17 support
-#               if defined(_HAS_CXX17) && _HAS_CXX17
-// We're using C++17, so let's use the normal version
-#                   define INCLUDE_STD_FILESYSTEM_EXPERIMENTAL 0
-#               endif
-#           endif
-
-// If the marco isn't defined yet, that means any of the other VS specific checks failed, so we need to use experimental
-#           ifndef INCLUDE_STD_FILESYSTEM_EXPERIMENTAL
-#               define INCLUDE_STD_FILESYSTEM_EXPERIMENTAL 1
-#           endif
-
-// Not on Visual Studio. Let's use the normal version
-#       else // #ifdef _MSC_VER
-#           define INCLUDE_STD_FILESYSTEM_EXPERIMENTAL 0
-#       endif
-
-// Check if the header "<filesystem>" exists
-#   elif __has_include(<experimental/filesystem>)
-#       define INCLUDE_STD_FILESYSTEM_EXPERIMENTAL 1
-
-// Fail if neither header is available with a nice error message
-#   else
-#       error Could not find system header "<filesystem>" or "<experimental/filesystem>"
-#   endif
-
-// We priously determined that we need the exprimental version
-#   if INCLUDE_STD_FILESYSTEM_EXPERIMENTAL
-// Include it
-#       include <experimental/filesystem>
-
-// We need the alias from std::experimental::filesystem to std::filesystem
-namespace std {
-    namespace filesystem = experimental::filesystem;
-}
-
-// We have a decent compiler and can use the normal version
-#   else
-// Include it
-#       include <filesystem>
-#   endif
-
-#endif // #ifndef INCLUDE_STD_FILESYSTEM_EXPERIMENTAL
-
+#if INCLUDE_STD_FILESYSTEM_EXPERIMENTAL
+    #include <experimental/filesystem>
+    // pull all identifiers from the experimental:: namespace
+    namespace std {
+        namespace filesystem = experimental::filesystem;
+    }
+#else
+    #include <filesystem>
+#endif
 
 #endif  // MARKET_ECOLOGY_FILESYSTEM_HPP
