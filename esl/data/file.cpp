b58de8292acfbf7e683eb8987bd7917b8194e1d4
Maarten P. Scholl <scholl.maarten@gmail.com>
2021-04-08 22:44:22 +0200

    fix filesystem portability issue

--- esl/data/file.cpp
+++ esl/data/file.cpp
@@ -6,7 +6,7 @@
 
 namespace esl::data {
 
-#ifndef __APPLE__
+
     file::file(const std::filesystem::path &name, const std::filesystem::path &prefix)
     : stream(file_)
     , file_((std::filesystem::create_directories(prefix), prefix / name ))
@@ -18,19 +18,4 @@ namespace esl::data {
             throw std::runtime_error(message_.str());
         }
     }
-#else
-    file::file(const std::string &name, const std::string &prefix)
-    : stream(file_)
-    //, file_((std::filesystem::create_directories(prefix), prefix / name ))
-    {
-        file_.open(prefix + "/" + name);
-
-        if(!file_.good()){
-            std::stringstream message_;
-            message_ << "Can't create output file " << (prefix + "/" + name);
-            LOG(errorlog) << message_.str() << std::endl;
-            throw std::runtime_error(message_.str());
-        }
-    }
-#endif
 }
