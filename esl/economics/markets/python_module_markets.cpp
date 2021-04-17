04413e4810f6d645c8d9ef20e503e65b9b7ec18d
Maarten P. Scholl <scholl.maarten@gmail.com>
2021-04-17 12:18:46 +0200

    boost macos detect python lib

--- esl/economics/markets/python_module_markets.cpp
+++ esl/economics/markets/python_module_markets.cpp
@@ -90,7 +90,7 @@ BOOST_PYTHON_MODULE(_markets)
 
     ////////////////////////////////////////////////////////////////////////////
 
-    class_<quote>("quote", no_init)
+    class_<quote, boost::noncopyable, boost::shared_ptr<quote> >("quote", no_init)
         .def("__init__", boost::python::make_constructor(construct_quote_from_price))
         .def("__init__", boost::python::make_constructor(construct_quote_from_exchange_rate))
         .add_property("price", &quote_helper_get_price, &quote_helper_set_price)
